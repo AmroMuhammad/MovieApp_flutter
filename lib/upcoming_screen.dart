75e999efbb81a80e25e6deb9102bbfb989f4b098
Amro <amrmu94@gmail.com>
2021-05-21 15:38:13 +0200

    finish hero transaction 3

--- lib/upcoming_screen.dart
+++ lib/upcoming_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +19,7 @@ class UpcomingScreen extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
+
 
   @override
   _UpcomingScreen createState() => _UpcomingScreen();
@@ -48,31 +50,125 @@ class _UpcomingScreen extends State<UpcomingScreen> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return FutureBuilder<Object>(
-        future: getMovies(),
-        builder: (context, snapshot) {
-          print(snapshot.connectionState);
-          if (snapshot.connectionState == ConnectionState.waiting)
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          return GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-              itemCount: movieResults.length,
-              itemBuilder: (context,index){
-                return GestureDetector(onTap: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MovieDetailsPage(
-                          movieDetails: movieResults[index],
-                        )),
-                  );
-                },child:Card(
-                    color: Colors.grey[800],
-                    child: Hero(child: Image.network(movieResults[index].posterPath,fit: BoxFit.fill),
-                      tag: "${movieResults[index].id}",))
-                  ,);
-              });
-        },
+      future: getMovies(),
+      builder: (context, snapshot) {
+        print(snapshot.connectionState);
+        if (snapshot.connectionState == ConnectionState.waiting)
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        return ListView.builder(
+            itemCount: movieResults.length,
+            itemBuilder: (context, int index) {
+              return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MovieDetailsPage(
+                            movieDetails: movieResults[index],
+                          )),
+                    );
+                  },
+                  child: Container(
+                      height: 100,
+                      color: const Color(0xFF0F1B40),
+                      child: Row(
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.fromLTRB(10, 10, 15, 10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Hero(
+                                    tag:"${movieResults[index].id}",
+                                    child:Image.network(
+                                        movieResults[index].posterPath,
+                                        height: 75.0,
+                                        width: 125.0,
+                                        fit: BoxFit.fill)),
+                              )),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 10, 0, 2.5),
+                                  child: Text(
+                                    movieResults[index].originalTitle,
+                                    style: TextStyle(color: Colors.white),
+                                  )),
+                              RatingBar.builder(
+                                initialRating: movieResults[index].voteAverage/2,
+                                minRating: 1,
+                                itemSize: 10,
+                                direction: Axis.horizontal,
+                                allowHalfRating: true,
+                                itemCount: 5,
+                                itemPadding:
+                                EdgeInsets.symmetric(horizontal: 4.0),
+                                itemBuilder: (context, _) => Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                onRatingUpdate: (rating) {
+                                  print(rating);
+                                },
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 2.5, 0, 2.5),
+                                  child: Text(
+                                      movieResults[index]
+                                          .voteCount
+                                          .toString() +
+                                          " reviews",
+                                      style: TextStyle(color: Colors.grey))),
+                              Container(
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.access_time,
+                                      color: Colors.blueGrey,
+                                      size: 15.0,
+                                    ),
+                                    Padding(
+                                        padding:
+                                        EdgeInsets.fromLTRB(2.5, 0, 0, 0),
+                                        child: Text(
+                                            movieResults[index]
+                                                .popularity
+                                                .toString(),
+                                            style: TextStyle(
+                                                color: Colors.grey))),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 2.5, 0, 0),
+                                  child: Container(
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.calendar_today,
+                                          color: Colors.blueGrey,
+                                          size: 15.0,
+                                        ),
+                                        Padding(
+                                            padding: EdgeInsets.fromLTRB(
+                                                2.5, 0, 0, 0),
+                                            child: Text(
+                                                movieResults[index]
+                                                    .releaseDate,
+                                                style: TextStyle(
+                                                    color: Colors.grey))),
+                                      ],
+                                    ),
+                                  )),
+                            ],
+                          )
+                        ],
+                      )));
+            });
+      },
     );
+
   }
 }
